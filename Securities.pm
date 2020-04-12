211a79657b7e7f61071d725825a9437e131e2b3f
Stefanos Manganaris <26627573+stefanos-manganaris@users.noreply.github.com>
2020-04-12 13:39:01 -0400

    updated securities from Vanguard

--- Securities.pm
+++ Securities.pm
@@ -7,7 +7,21 @@
   "92202E748" => "Vanguard Institutional Target Retirement 2045 Institutional",
   "87244W789" => "TIAA-CREF Ret Real Est Sec F",
   "87244W748" => "TIAA-CREF Ret Intl Equity F",
-  "87244W698" => "TIAA-CREF Ret S&P 500 Index F"
+  "87244W698" => "TIAA-CREF Ret S&P 500 Index F",
+  "922908710" => "Vanguard 500 Index Fund Admiral Shares",
+  "922906300" => "Vanguard Federal Money Market Fund",
+  "922906201" => "Money Mkt Reserves (Prime)",
+  "922031307" => "Vanguard GNMA",
+  "922031836" => "Vanguard ST Inv Grade",
+  "921946109" => "Vanguard Selected Value (VANGUARD)",
+  "78462F103" => "SPDR'S",
+  "458140100" => "Intel",
+  "369604103" => "General Electric",
+  "922908645" => "Vanguard Mid-Cap Index (VIMAX)",
+  "929740108" => "Wabtec",
+  "92202V195" => "Vanguard Target Retirement 2060 Trust II",
+  "921936209" => "Vanguard PRIMECAP Fund Admiral Shares",
+  "922908363" => "VOO"
 );
 
 1;
